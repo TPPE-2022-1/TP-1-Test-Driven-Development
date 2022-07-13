143b77a655e5df60f55248601cea841467ad082f
Davi Antônio da Silva Santos <antoniossdavi@gmail.com>
2022-07-13 16:32:23 -0300

    Triangular teste de valor de desconto de hora cheia

--- src/test/RegistraValorHoraCheiaEstacionamentoTest.java
+++ src/test/RegistraValorHoraCheiaEstacionamentoTest.java
@@ -22,5 +22,32 @@ class RegistraValorHoraCheiaEstacionamentoTest {
 		assertDoesNotThrow(() -> estacionamentoBuilder.setDescontoHoraCheia(0.00));
 		assertEquals(0.00, estacionamentoBuilder.getDescontoHoraCheia());
 	}
+	
+	@Test
+	@Tag("TesteFuncional")
+	void testAdicionaValorHoraCheia10() {
+		EstacionamentoBuilder estacionamentoBuilder = new EstacionamentoBuilder();
+
+		assertDoesNotThrow(() -> estacionamentoBuilder.setDescontoHoraCheia(10.00));
+		assertEquals(10.00, estacionamentoBuilder.getDescontoHoraCheia());
+	}
+	
+	@Test
+	@Tag("TesteFuncional")
+	void testAdicionaValorHoraCheia1025() {
+		EstacionamentoBuilder estacionamentoBuilder = new EstacionamentoBuilder();
+
+		assertDoesNotThrow(() -> estacionamentoBuilder.setDescontoHoraCheia(10.25));
+		assertEquals(10.25, estacionamentoBuilder.getDescontoHoraCheia());
+	}
+	
+	@Test
+	@Tag("TesteFuncional")
+	void testAdicionaValorHoraCheia9999() {
+		EstacionamentoBuilder estacionamentoBuilder = new EstacionamentoBuilder();
+
+		assertDoesNotThrow(() -> estacionamentoBuilder.setDescontoHoraCheia(99.99));
+		assertEquals(99.99, estacionamentoBuilder.getDescontoHoraCheia());
+	}
 
 }
