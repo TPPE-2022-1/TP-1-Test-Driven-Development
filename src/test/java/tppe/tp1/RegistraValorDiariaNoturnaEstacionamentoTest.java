a628f5d8b83ae6225ef13615f3ba829af42bdc6a
Davi Antônio da Silva Santos <antoniossdavi@gmail.com>
2022-07-15 15:42:13 -0300

    Triangular adição de desconto de diária noturna

--- src/test/java/tppe/tp1/RegistraValorDiariaNoturnaEstacionamentoTest.java
+++ src/test/java/tppe/tp1/RegistraValorDiariaNoturnaEstacionamentoTest.java
@@ -20,4 +20,23 @@ class RegistraValorDiariaNoturnaEstacionamentoTest {
 		assertDoesNotThrow(() -> estacionamentoBuilder.setDescontoDiariaNoturna(0.00));
 		assertEquals(0.00, estacionamentoBuilder.getDescontoDiariaNoturna());
 	}
+	
+	@Test
+	@Tag("TesteFuncional")
+	void testAdicionaValorDiariaNoturna10() {
+		EstacionamentoBuilder estacionamentoBuilder = new EstacionamentoBuilder();
+
+		assertDoesNotThrow(() -> estacionamentoBuilder.setDescontoDiariaNoturna(10.00));
+		assertEquals(10.00, estacionamentoBuilder.getDescontoDiariaNoturna());
+	}
+	
+	@Test
+	@Tag("TesteFuncional")
+	void testAdicionaValorDiariaNoturna9999() {
+		EstacionamentoBuilder estacionamentoBuilder = new EstacionamentoBuilder();
+
+		assertDoesNotThrow(() -> estacionamentoBuilder.setDescontoDiariaNoturna(99.99));
+		assertEquals(99.99, estacionamentoBuilder.getDescontoDiariaNoturna());
+	}
+
 }
