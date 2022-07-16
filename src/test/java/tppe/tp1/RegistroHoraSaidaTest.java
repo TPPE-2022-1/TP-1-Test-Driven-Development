2844e86353e9af3f6bf684faac627d2931798cf2
Antonio Aldisio <aldisiofilho@gmail.com>
2022-07-16 17:26:39 -0300

    Parametrização dos testes horario de saida
    Co-Authored-By: Fernando Miranda <ilusdasilva@gmail.com>

--- src/test/java/tppe/tp1/RegistroHoraSaidaTest.java
+++ src/test/java/tppe/tp1/RegistroHoraSaidaTest.java
@@ -3,47 +3,38 @@ package tppe.tp1;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.time.LocalTime;
+import java.util.stream.Stream;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Tag;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 import tppe.tp1.acesso.AcessoBuilder;
 
 public class RegistroHoraSaidaTest {
-
+	
 	@BeforeEach
 	void setUp() throws Exception {
 	}
 	
-	@Test
+	static Stream<Arguments> geraHoraSaida() {
+		return Stream.of(Arguments.of("08:30"),
+				Arguments.of("14:30"),
+				Arguments.of("19:19")
+				);
+	}
+	
+	@ParameterizedTest
+	@MethodSource("geraHoraSaida")
 	@Tag("TesteFuncional")
-	void testRegistroHoraSaida01() {
+	void testRegistroHoraSaida(String saida) {
 		AcessoBuilder acessoBuilder = new AcessoBuilder();
-		LocalTime horaSaida = LocalTime.parse("08:30");
+		LocalTime horaSaida = LocalTime.parse(saida);
 		acessoBuilder.setHoraSaida(horaSaida);
 		assertEquals(horaSaida, acessoBuilder.getHoraSaida());
 		
 	}
-	
-	@Test
-	@Tag("TesteFuncional")
-	void testRegistroHoraSaida02() {
-		AcessoBuilder acessoBuilder = new AcessoBuilder();
-		LocalTime horaSaida = LocalTime.parse("11:30");
-		acessoBuilder.setHoraSaida(horaSaida);
-		assertEquals(horaSaida, acessoBuilder.getHoraSaida());
-		
-	}
-	
-	
-	@Test
-	@Tag("TesteFuncional")
-	void testRegistroHoraSaida03() {
-		AcessoBuilder acessoBuilder = new AcessoBuilder();
-		LocalTime horaSaida = LocalTime.parse("23:29");
-		acessoBuilder.setHoraSaida(horaSaida);
-		assertEquals(horaSaida, acessoBuilder.getHoraSaida());
-		
-	}
+
 }
