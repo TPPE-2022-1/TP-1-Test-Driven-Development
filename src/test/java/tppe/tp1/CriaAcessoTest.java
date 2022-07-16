6bc4eeeb3036f4d91a6b72c9935b6647b3f8a943
Fernando <fernando.calil@terceiro.rnp.br>
2022-07-16 16:37:26 -0300

    Parametrização dos testes de criação de um acesso

--- src/test/java/tppe/tp1/CriaAcessoTest.java
+++ src/test/java/tppe/tp1/CriaAcessoTest.java
@@ -3,72 +3,48 @@ package tppe.tp1;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.time.LocalTime;
+import java.util.stream.Stream;
 
 import org.junit.jupiter.api.Tag;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 import tppe.tp1.acesso.Acesso;
 import tppe.tp1.acesso.AcessoBuilder;
 
 public class CriaAcessoTest {
 
-	@Test
+	
+	@ParameterizedTest
+	@MethodSource("criaAcessos")
 	@Tag("TesteFuncional")
-	void testCriaAcesso() throws Exception  {
+	void testCriaAcesso(String placa, String entrada, String saida) {
 		AcessoBuilder acessoBuilder = new AcessoBuilder();
-		
-		String placa = "JJO0830";
-		LocalTime horaEntrada = LocalTime.of(14, 30);
-		LocalTime horaSaida = LocalTime.of(07, 30);
-		
+
+		LocalTime horaEntrada = LocalTime.parse(entrada);
+		LocalTime horaSaida = LocalTime.parse(saida);
+
 		acessoBuilder.setPlaca(placa);
 		acessoBuilder.setHoraEntrada(horaEntrada);
 		acessoBuilder.setHoraSaida(horaSaida);
-		
+
 		Acesso acesso = acessoBuilder.build();
 		
 		assertEquals(placa, acesso.getPlaca());
 		assertEquals(horaEntrada, acesso.getHoraEntrada());
 		assertEquals(horaSaida, acesso.getHoraSaida());
+	}
+	
+	static Stream<Arguments> criaAcessos() {
+		return Stream.of(
+					Arguments.of("JHD9698", "22:40", "23:30"),
+					Arguments.of("JGQ4642", "12:40", "18:00"),
+					Arguments.of("JGF6820", "10:30", "22:00")
+				);
 	}
 
-	@Test
-	@Tag("TesteFuncional")
-	void testCriaAcessoD() throws Exception  {
-		AcessoBuilder acessoBuilder = new AcessoBuilder();
-		
-		String placa = "JGC2830";
-		LocalTime horaEntrada = LocalTime.of(10, 30);
-		LocalTime horaSaida = LocalTime.of(15, 30);
-		
-		acessoBuilder.setPlaca(placa);
-		acessoBuilder.setHoraEntrada(horaEntrada);
-		acessoBuilder.setHoraSaida(horaSaida);
-		
-		Acesso acesso = acessoBuilder.build();
-		
-		assertEquals(placa, acesso.getPlaca());
-		assertEquals(horaEntrada, acesso.getHoraEntrada());
-		assertEquals(horaSaida, acesso.getHoraSaida());
-	}
 
-	@Test
-	@Tag("TesteFuncional")
-	void testCriaAcessoT() throws Exception  {
-		AcessoBuilder acessoBuilder = new AcessoBuilder();
-		
-		String placa = "JHX1785";
-		LocalTime horaEntrada = LocalTime.of(7, 30);
-		LocalTime horaSaida = LocalTime.of(15, 30);
-		
-		acessoBuilder.setPlaca(placa);
-		acessoBuilder.setHoraEntrada(horaEntrada);
-		acessoBuilder.setHoraSaida(horaSaida);
-		
-		Acesso acesso = acessoBuilder.build();
-		
-		assertEquals(placa, acesso.getPlaca());
-		assertEquals(horaEntrada, acesso.getHoraEntrada());
-		assertEquals(horaSaida, acesso.getHoraSaida());
-	}
+
+
 }
