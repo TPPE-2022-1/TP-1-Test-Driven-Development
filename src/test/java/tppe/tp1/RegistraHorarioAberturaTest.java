aeb7f97f7aa2752f46793d2be8c9650621715b55
GuilhermeBraz <gvcerveirabraz@gmail.com>
2022-07-16 18:30:43 -0300

    parametrizar RegistraHorarioAberturaTest

--- src/test/java/tppe/tp1/RegistraHorarioAberturaTest.java
+++ src/test/java/tppe/tp1/RegistraHorarioAberturaTest.java
@@ -4,40 +4,39 @@ import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.time.LocalTime;
+import java.util.stream.Stream;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 import tppe.tp1.estacionamento.EstacionamentoBuilder;
 
 public class RegistraHorarioAberturaTest {
-
-	@Test
-	@Tag("TesteFuncional")
-	void testaInsereHorarioAbertura0600() {
-		EstacionamentoBuilder estacionamentoBuilder = new EstacionamentoBuilder();
-
-		LocalTime horarioAbertura = LocalTime.parse("06:00");
-		assertDoesNotThrow(() -> estacionamentoBuilder.setHorarioAbertura(horarioAbertura));
-		assertEquals(horarioAbertura, estacionamentoBuilder.getHorarioAbertura());
+	
+	@BeforeEach
+	public void setup() throws Exception{
 	}
-
-	@Test
-	@Tag("TesteFuncional")
-	void testaInsereHorarioAbertura0630() {
-		EstacionamentoBuilder estacionamentoBuilder = new EstacionamentoBuilder();
-
-		LocalTime horarioAbertura = LocalTime.parse("06:30");
-		assertDoesNotThrow(() -> estacionamentoBuilder.setHorarioAbertura(horarioAbertura));
-		assertEquals(horarioAbertura, estacionamentoBuilder.getHorarioAbertura());
+	
+	
+//  Parametrizacao de teste
+	static Stream<Arguments> getHorarioAbertura() {
+		return Stream.of(Arguments.of("06:00"),
+                     	Arguments.of("06:30"),
+                     	Arguments.of("00:00")
+				);
 	}
-
-	@Test
+	
+	@ParameterizedTest
+	@MethodSource("getHorarioAbertura")
 	@Tag("TesteFuncional")
-	void testaInsereHorarioAbertura0000() {
+	void testaInsereHorarioAbertura(String entrada) {
 		EstacionamentoBuilder estacionamentoBuilder = new EstacionamentoBuilder();
 
-		LocalTime horarioAbertura = LocalTime.parse("00:00");
+		LocalTime horarioAbertura = LocalTime.parse(entrada);
 		assertDoesNotThrow(() -> estacionamentoBuilder.setHorarioAbertura(horarioAbertura));
 		assertEquals(horarioAbertura, estacionamentoBuilder.getHorarioAbertura());
 	}
