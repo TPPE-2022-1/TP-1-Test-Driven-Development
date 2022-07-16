962012105a51d0d209a557ab82257fb32b170ac3
Antonio Aldisio <aldisiofilho@gmail.com>
2022-07-16 16:47:53 -0300

    Parametrização dos testes de hora de entrada

--- src/test/java/tppe/tp1/RegistroHoraEntradaTest.java
+++ src/test/java/tppe/tp1/RegistroHoraEntradaTest.java
@@ -3,9 +3,13 @@ package tppe.tp1;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.time.LocalTime;
+import java.util.stream.Stream;
 
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 import tppe.tp1.acesso.AcessoBuilder;
 
@@ -14,30 +18,24 @@ public class RegistroHoraEntradaTest {
 	void setUp() throws Exception {
 	}
 	
-	@Test
-	void testRegistroHoraEntrada01() {
+	static Stream<Arguments> geraHoraEntrada() {
+		return Stream.of(Arguments.of("08:30"),
+				Arguments.of("12:30"),
+				Arguments.of("19:19")
+				);
+	}
+	
+	
+	@ParameterizedTest
+	@MethodSource("geraHoraEntrada")
+	@Tag("TesteFuncional")
+	void testRegistroHoraEntrada01(String entrada) {
 		AcessoBuilder acessoBuilder = new AcessoBuilder();
-		LocalTime horaEntrada = LocalTime.parse("08:30");
+		LocalTime horaEntrada = LocalTime.parse(entrada);
 		acessoBuilder.setHoraEntrada(horaEntrada);
 		assertEquals(horaEntrada, acessoBuilder.getHoraEntrada());
 		
 	}
 
-	@Test
-	void testRegistroHoraEntrada02() {
-		AcessoBuilder acessoBuilder = new AcessoBuilder();
-		LocalTime horaEntrada = LocalTime.parse("12:12");
-		acessoBuilder.setHoraEntrada(horaEntrada);
-		assertEquals(horaEntrada, acessoBuilder.getHoraEntrada());
-		
-	}
 	
-	@Test
-	void testRegistroHoraEntrada03() {
-		AcessoBuilder acessoBuilder = new AcessoBuilder();
-		LocalTime horaEntrada = LocalTime.parse("19:59");
-		acessoBuilder.setHoraEntrada(horaEntrada);
-		assertEquals(horaEntrada, acessoBuilder.getHoraEntrada());
-		
-	}
 }
