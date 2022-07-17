bbf212642caa509442fa0bb6b406b49238b6dec4
Fernando <fernando.calil@terceiro.rnp.br>
2022-07-17 10:58:01 -0300

    Teste Funcional - Checagem de tipo de acessoEvento - falsificação
    Co-authored-by: antonioaldisio <aldisiofilho@gmail.com>

---
+++ src/test/java/tppe/tp1/ChecaTipoEventoTest.java
@@ -0,0 +1,36 @@
+package tppe.tp1;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.time.LocalTime;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+
+import tppe.tp1.acesso.Acesso;
+import tppe.tp1.acesso.AcessoBuilder;
+
+public class ChecaTipoEventoTest {
+
+
+	Acesso acesso;
+	AcessoBuilder a = new AcessoBuilder();
+	
+	@BeforeEach
+	void setup() throws Exception {
+		a.setPlaca("JHS1227");
+		a.setHoraEntrada(LocalTime.of(8, 0));
+		a.setHoraSaida(LocalTime.of(12, 0));
+	}
+	
+	@Test
+	@Tag("TesteFuncional")
+	void checaTipoEntradaD() throws Exception {
+		a.setTipoAcesso("Evento");
+		acesso = a.build();
+		assertTrue(acesso.isEvento(acesso.getTipoAcesso()));
+	}
+	
+	
+}
