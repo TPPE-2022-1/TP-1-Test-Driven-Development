b81b162b80eba4d558cc9a933fe0fba3dcf610a1
Fernando <fernando.calil@terceiro.rnp.br>
2022-07-17 10:47:14 -0300

    Teste Funcional - Checagem de tipo de entrada - Falsificação
    Co-authored-by: antonioaldisio <aldisiofilho@gmail.com>

---
+++ src/test/java/tppe/tp1/ChecaTipoMensalistaTest.java
@@ -0,0 +1,33 @@
+package tppe.tp1;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.time.LocalTime;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+
+import tppe.tp1.acesso.Acesso;
+import tppe.tp1.acesso.AcessoBuilder;
+
+public class ChecaTipoMensalistaTest {
+
+	Acesso acesso;
+	AcessoBuilder a = new AcessoBuilder();
+	
+	@BeforeEach
+	void setup() throws Exception {
+		a.setPlaca("JIL2809");
+		a.setHoraEntrada(LocalTime.of(12, 0));
+		a.setHoraSaida(LocalTime.of(15, 0));
+	}
+	
+	@Test
+	@Tag("TesteFuncional")
+	void checaTipoEntrada() throws Exception {
+		a.setTipoAcesso("Mensalista");
+		acesso = a.build();
+		assertTrue(acesso.isMensalista(acesso.getTipoAcesso()));
+	}
+}
