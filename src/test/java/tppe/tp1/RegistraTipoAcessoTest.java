9aa0d99af0e02a11c97d28386a3c640db3a67ff8
Fernando <fernando.calil@terceiro.rnp.br>
2022-07-17 16:30:44 -0300

    Parametrização do teste de tipo de acesso

--- src/test/java/tppe/tp1/RegistraTipoAcessoTest.java
+++ src/test/java/tppe/tp1/RegistraTipoAcessoTest.java
@@ -3,35 +3,37 @@ package tppe.tp1;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import java.util.stream.Stream;
+
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 import tppe.tp1.acesso.AcessoBuilder;
 import tppe.tp1.acesso.exceptions.AcessoEventoInvalidoException;
 import tppe.tp1.acesso.exceptions.AcessoEventoVazioException;
 
 public class RegistraTipoAcessoTest {
+
+	AcessoBuilder acesso;
 	
-	@Test
+	
+	@ParameterizedTest
+	@MethodSource("geraTipoAcesso")
 	@Tag("TesteFuncional")
-	void testaInsereMensalista() throws Exception {
-		AcessoBuilder acesso = new AcessoBuilder();
-		String tipoAcesso = "Mensalista";
-		
+	void testaInsereMensalista(String tipoAcesso) throws Exception {
+		acesso = new AcessoBuilder();
 		acesso.setTipoAcesso(tipoAcesso);
+
 		assertEquals(tipoAcesso, acesso.getTipoAcesso());
 	}
-	
-	@Test
-	@Tag("TesteFuncional")
-	void testaInsereEvento() throws Exception {
-		AcessoBuilder acesso = new AcessoBuilder();
-		String tipoAcesso = "Evento";
-		
-		acesso.setTipoAcesso(tipoAcesso);
-		assertEquals(tipoAcesso, acesso.getTipoAcesso());
+
+	static Stream<Arguments> geraTipoAcesso() {
+		return Stream.of(Arguments.of("Mensalista"),
+				Arguments.of("Evento"));
 	}
-	
 	
 	@Test
 	@Tag("TesteExcecao")
