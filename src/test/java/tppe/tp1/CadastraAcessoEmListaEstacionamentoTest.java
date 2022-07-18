2b79ceefe874514cd6d7b73fa94b76985d1e5267
Davi Antônio da Silva Santos <antoniossdavi@gmail.com>
2022-07-17 22:03:17 -0300

    Verificar métodos que não deveriam lançar exceções no teste da lista

--- src/test/java/tppe/tp1/CadastraAcessoEmListaEstacionamentoTest.java
+++ src/test/java/tppe/tp1/CadastraAcessoEmListaEstacionamentoTest.java
@@ -1,16 +1,14 @@
 package tppe.tp1;
 
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 import java.time.LocalTime;
-
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
-
 
 import tppe.tp1.acesso.Acesso;
 import tppe.tp1.acesso.AcessoBuilder;
@@ -19,60 +17,63 @@ import tppe.tp1.estacionamento.EstacionamentoBuilder;
 
 public class CadastraAcessoEmListaEstacionamentoTest {
 	private Estacionamento estacionamento;
-	
+
 	@BeforeEach
-	void setup() throws Exception {
+	void setup() {
 		EstacionamentoBuilder e = new EstacionamentoBuilder();
-		e.setId(0);
-		e.setValorFracao(30.00);
-		e.setDescontoHoraCheia(15.00);
-		e.setValorDiariaDiurna(120.00);
-		e.setDescontoDiariaNoturna(45.00);
-		e.setHorarioEntradaDiariaNoturna(LocalTime.of(19, 0));
-		e.setHorarioSaidaDiariaNoturna(LocalTime.of(8, 0));
-		e.setValorMensalidade(600.00);
-		e.setValorEvento(50.00);
-		e.setHorarioAbertura(LocalTime.of(6, 0));
-		e.setHorarioEncerramento(LocalTime.of(22, 0));
-		e.setCapacidade(300);
-		e.setRetornoContratante(50.00);
-		this.estacionamento = e.build();
+
+		assertDoesNotThrow(() -> {
+			e.setId(0);
+			e.setValorFracao(30.00);
+			e.setDescontoHoraCheia(15.00);
+			e.setValorDiariaDiurna(120.00);
+			e.setDescontoDiariaNoturna(45.00);
+			e.setHorarioEntradaDiariaNoturna(LocalTime.of(19, 0));
+			e.setHorarioSaidaDiariaNoturna(LocalTime.of(8, 0));
+			e.setValorMensalidade(600.00);
+			e.setValorEvento(50.00);
+			e.setHorarioAbertura(LocalTime.of(6, 0));
+			e.setHorarioEncerramento(LocalTime.of(22, 0));
+			e.setCapacidade(300);
+			e.setRetornoContratante(50.00);
+			this.estacionamento = e.build();
+		});
 	}
-	
+
 	@Test
 	@Tag("TesteFuncional")
-	void testCadastraAcessoEmListaEstacionamento() throws Exception {		
+	void testCadastraAcessoEmListaEstacionamento() {
 		AcessoBuilder acessoBuilder = new AcessoBuilder();
 		Acesso acesso;
-		
-		acessoBuilder.setPlaca("JHD9698");
+
+		assertDoesNotThrow(() -> acessoBuilder.setPlaca("JHD9698"));
 		acessoBuilder.setHoraEntrada(LocalTime.of(22, 40));
 		acessoBuilder.setHoraSaida(LocalTime.of(23, 30));
-		
-		acesso = acessoBuilder.build();
-		
+
+		acesso = assertDoesNotThrow(() -> acessoBuilder.build());
+
 		estacionamento.addAcesso(acesso);
 		assertEquals(acesso, estacionamento.getAcesso(0));
-		
+
 	}
-	
+
 	@Test
 	@Tag("TesteFuncional")
-	void testCadastraAcessoEmListaEstacionamentoCheia() throws Exception {
-		AcessoBuilder  acessoBuilder2 = new AcessoBuilder();
+	void testCadastraAcessoEmListaEstacionamentoCheia() {
+		AcessoBuilder acessoBuilder2 = new AcessoBuilder();
 		AcessoBuilder acessoBuilder = new AcessoBuilder();
 		Acesso acesso, acesso2;
-		
-		acessoBuilder.setPlaca("GHJ7653");
+
+		assertDoesNotThrow(() -> acessoBuilder.setPlaca("GHJ7653"));
 		acessoBuilder.setHoraEntrada(LocalTime.of(12, 40));
 		acessoBuilder.setHoraSaida(LocalTime.of(15, 30));
-		acessoBuilder2.setPlaca("XXX1234");
+		assertDoesNotThrow(() -> acessoBuilder2.setPlaca("XXX1234"));
 		acessoBuilder2.setHoraEntrada(LocalTime.of(18, 40));
 		acessoBuilder2.setHoraSaida(LocalTime.of(20, 30));
-		
-		acesso = acessoBuilder.build();
-		acesso2 = acessoBuilder.build();
-		
+
+		acesso = assertDoesNotThrow(() -> acessoBuilder.build());
+		acesso2 = assertDoesNotThrow(() -> acessoBuilder.build());
+
 		estacionamento.addAcesso(acesso);
 		estacionamento.addAcesso(acesso2);
 		assertFalse(estacionamento.getListaAcessos().isEmpty());
