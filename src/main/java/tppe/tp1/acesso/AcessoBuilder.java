a4e8dfe64cf23ec299c0ac9ec8864690caedd686
Davi Antônio da Silva Santos <antoniossdavi@gmail.com>
2022-07-18 12:53:36 -0300

    Inicia Migração do registro de entrada para incluir data

--- src/main/java/tppe/tp1/acesso/AcessoBuilder.java
+++ src/main/java/tppe/tp1/acesso/AcessoBuilder.java
@@ -1,24 +1,25 @@
 package tppe.tp1.acesso;
 
-import java.time.LocalTime;
+import java.time.Duration;
+import java.time.LocalDateTime;
+import java.time.Period;
 
 import tppe.tp1.acesso.exceptions.AcessoEventoInvalidoException;
 import tppe.tp1.acesso.exceptions.AcessoEventoVazioException;
 import tppe.tp1.acesso.exceptions.AcessoPlacaInvalidaException;
 import tppe.tp1.acesso.exceptions.AcessoPlacaVazioException;
 import tppe.tp1.exceptions.DescricaoEmBrancoException;
-import tppe.tp1.estacionamento.EstacionamentoBuilder;
-
+import tppe.tp1.estacionamento.Estacionamento;
 
 public class AcessoBuilder {
-	
+
 	private String placa;
-	private LocalTime horaEntrada;
-	private LocalTime horaSaida;
+	private LocalDateTime horaEntrada;
+	private LocalDateTime horaSaida;
 	private String tipoAcesso;
 	private Float valorAcesso;
-	private Float valorContratante;	
-	
+	private Float valorContratante;
+
 	public Acesso build() throws DescricaoEmBrancoException {
 		if (horaEntrada == null) {
 			throw new DescricaoEmBrancoException("Hora de entrada não pode ser nula");
@@ -32,7 +33,7 @@ public class AcessoBuilder {
 //		if (valorContratante == null) {
 //			throw new DescricaoEmBrancoException("Valor do contratante não pode ser nulo");
 //		}
-		
+
 		return new Acesso(this);
 	}
 
@@ -41,11 +42,11 @@ public class AcessoBuilder {
 		return placa;
 	}
 
-	public LocalTime getHoraEntrada() {
+	public LocalDateTime getHoraEntrada() {
 		return horaEntrada;
 	}
 
-	public LocalTime getHoraSaida() {
+	public LocalDateTime getHoraSaida() {
 		return horaSaida;
 	}
 
@@ -61,53 +62,76 @@ public class AcessoBuilder {
 		return valorContratante;
 	}
 
-	public Boolean isDiariaDiurna(EstacionamentoBuilder e) {
-		LocalTime entrada = getHoraEntrada();
-		LocalTime saida = getHoraSaida();
-		Boolean entrouNoite = false;
-		Boolean saiuNoite = false;
-		if(saida.compareTo(entrada) < 0) {
-			if(saida.toSecondOfDay() - e.getHorarioSaidaDiariaNoturna().toSecondOfDay()
-					>= LocalTime.of(8, 46).toSecondOfDay())
-				return true; // ficou toda a noite mais 8:46 horas de dia
-			if(e.getHorarioEntradaDiariaNoturna().toSecondOfDay() - entrada.toSecondOfDay()
-					>= LocalTime.of(8, 46).toSecondOfDay())
-				return true; // ficou mais de 8:46 horas no dia e virou a noite
-		}
-		if(getHoraEntrada().compareTo(e.getHorarioEntradaDiariaNoturna()) > 0 ||
-		   getHoraEntrada().compareTo(e.getHorarioSaidaDiariaNoturna()) < 0) {
-			entrada = e.getHorarioSaidaDiariaNoturna();
-			entrouNoite = true;
-		}
-		if(getHoraSaida().compareTo(e.getHorarioSaidaDiariaNoturna()) < 0||
-		   getHoraSaida().compareTo(e.getHorarioEntradaDiariaNoturna()) > 0) {
-			saida = e.getHorarioEntradaDiariaNoturna();
-			saiuNoite = true;
-		}
-		if(entrouNoite && saiuNoite)
-			return false;  // ficou somente o periodo noturno
-		if(saida.toSecondOfDay() - entrada.toSecondOfDay() < LocalTime.of(8, 46).toSecondOfDay())
-				return false; // nao completou 8:46 horas dirunas
-		return true; // completou 9 hroas diurnas
+	public Boolean isDiariaDiurna(Estacionamento e) {
+		LocalDateTime entrada = getHoraEntrada();
+		LocalDateTime saida = getHoraSaida();
+		LocalDateTime horarioEntradaNoturna = LocalDateTime.of(entrada.toLocalDate(),
+				e.getHorarioEntradaDiariaNoturna());
+		// não pode virar o dia
+		LocalDateTime horarioSaidaNoturna = LocalDateTime.of(entrada.toLocalDate(), e.getHorarioSaidaDiariaNoturna());
+
+		Duration duration = Duration.between(entrada, saida);
+
+		return (duration.toHours() >= 9) && entrada.isAfter(horarioSaidaNoturna)
+				&& saida.isBefore(horarioEntradaNoturna);
+
+//		LocalDateTime entrada = getHoraEntrada();
+//		LocalDateTime saida = getHoraSaida();
+//		Boolean entrouNoite = false;
+//		Boolean saiuNoite = false;
+//		if(saida.compareTo(entrada) < 0) {
+//			if(saida.toSecondOfDay() - e.getHorarioSaidaDiariaNoturna().toSecondOfDay()
+//					>= LocalTime.of(8, 46).toSecondOfDay())
+//				return true; // ficou toda a noite mais 8:46 horas de dia
+//			if(e.getHorarioEntradaDiariaNoturna().toSecondOfDay() - entrada.toSecondOfDay()
+//					>= LocalTime.of(8, 46).toSecondOfDay())
+//				return true; // ficou mais de 8:46 horas no dia e virou a noite
+//		}
+//		if(getHoraEntrada().compareTo(e.getHorarioEntradaDiariaNoturna()) > 0 ||
+//		   getHoraEntrada().compareTo(e.getHorarioSaidaDiariaNoturna()) < 0) {
+//			entrada = e.getHorarioSaidaDiariaNoturna();
+//			entrouNoite = true;
+//		}
+//		if(getHoraSaida().compareTo(e.getHorarioSaidaDiariaNoturna()) < 0||
+//		   getHoraSaida().compareTo(e.getHorarioEntradaDiariaNoturna()) > 0) {
+//			saida = e.getHorarioEntradaDiariaNoturna();
+//			saiuNoite = true;
+//		}
+//		if(entrouNoite && saiuNoite)
+//			return false;  // ficou somente o periodo noturno
+//		if(saida.toSecondOfDay() - entrada.toSecondOfDay() < LocalTime.of(8, 46).toSecondOfDay())
+//				return false; // nao completou 8:46 horas dirunas
+//		return true; // completou 9 hroas diurnas
 	}
-	
-	public Boolean isDiariaNoturna(EstacionamentoBuilder e) {
-		LocalTime entrada = getHoraEntrada();
-		LocalTime saida = getHoraSaida();
-		if(saida.compareTo(entrada) > 0) {
-			if(entrada.compareTo(e.getHorarioSaidaDiariaNoturna()) < 0)
-				return true; // entrou ainda no periodo noturno
-			if(isDiariaDiurna(e))
-				return false; // horario já pago na diaria noturna
-			if(saida.compareTo(e.getHorarioEntradaDiariaNoturna()) > 0)
-				return true; // ficou a noite
-			return false;
-		}
-		return true; // virou o dia
+
+	public Boolean isDiariaNoturna(Estacionamento e) {
+		LocalDateTime entrada = getHoraEntrada();
+		LocalDateTime saida = getHoraSaida();
+
+		LocalDateTime horarioEntradaNoturna = LocalDateTime.of(entrada.toLocalDate(),
+				e.getHorarioEntradaDiariaNoturna());
+		LocalDateTime horarioSaidaNoturna = LocalDateTime.of(entrada.toLocalDate().plusDays(1), e.getHorarioSaidaDiariaNoturna());
+
+		Period estadia = Period.between(entrada.toLocalDate(), saida.toLocalDate());
+
+		return (estadia.getDays() <= 1) && entrada.isAfter(horarioEntradaNoturna) && saida.isBefore(horarioSaidaNoturna);
+
+//		LocalDateTime entrada = getHoraEntrada();
+//		LocalDateTime saida = getHoraSaida();
+//		if(saida.compareTo(entrada) > 0) {
+//			if(entrada.compareTo(e.getHorarioSaidaDiariaNoturna()) < 0)
+//				return true; // entrou ainda no periodo noturno
+//			if(isDiariaDiurna(e))
+//				return false; // horario já pago na diaria noturna
+//			if(saida.compareTo(e.getHorarioEntradaDiariaNoturna()) > 0)
+//				return true; // ficou a noite
+//			return false;
+//		}
+//		return true; // virou o dia
 	}
 	// Fim Get
-	
-	//	SET
+
+	// SET
 	public void setValorContratante(Float valorContratante) {
 		this.valorContratante = valorContratante;
 	}
@@ -115,7 +139,7 @@ public class AcessoBuilder {
 	public void setValorAcesso(Float valorAcesso) {
 		this.valorAcesso = valorAcesso;
 	}
-	
+
 	public void setTipoAcesso(String tipoAcesso) throws AcessoEventoInvalidoException, AcessoEventoVazioException {
 		if (tipoAcesso.trim().equalsIgnoreCase("Mensalista"))
 			this.tipoAcesso = "Mensalista";
@@ -127,22 +151,22 @@ public class AcessoBuilder {
 			throw new AcessoEventoVazioException();
 	}
 
-	public void setHoraSaida(LocalTime horaSaida) {
+	public void setHoraSaida(LocalDateTime horaSaida) {
 		this.horaSaida = horaSaida;
 	}
 
 	public void setPlaca(String placa) throws AcessoPlacaInvalidaException, AcessoPlacaVazioException {
-		if(placa.trim().isEmpty()) {
+		if (placa.trim().isEmpty()) {
 			throw new AcessoPlacaVazioException();
 		}
-		if(placa.trim().length() != 7) {
+		if (placa.trim().length() != 7) {
 			throw new AcessoPlacaInvalidaException();
 		}
 		this.placa = placa;
 	}
 
-	public void setHoraEntrada(LocalTime horaEntrada) {
+	public void setHoraEntrada(LocalDateTime horaEntrada) {
 		this.horaEntrada = horaEntrada;
 	}
-	//	FIM SET
+	// FIM SET
 }
